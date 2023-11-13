482b2c8b4cbbf0e4c2ead49822336e887bca685b
Bastien Chevallier <cheval_b@etna-alternance.net>
2023-11-13 14:26:10 +0100

    Update dekuple.ts

--- src/dekuple.ts
+++ src/dekuple.ts
@@ -1,8 +1,14 @@
-document.getElementsByName("mobilephone")[0].addEventListener("change", (e) => {
-  const phoneNumber = e.target.value;
-  // Get only numbers
-  const numbers = phoneNumber.replace(/\D/g, "");
-  if (numbers.length <= 10) return numbers;
-  // Get 9 last digits and add 0 at the beginning to remove international code
-  e.target.value = "0" + numbers.slice(numbers.length - 9);
-});
+var intervalID = setInterval(function() {
+  var element = document.getElementsByName("mobilephone")
+  if (element && element[0]) {
+    element[0].addEventListener("change", (e) => {
+      var phoneNumber = e.target.value;
+      // Get only numbers
+      var numbers = phoneNumber.replace(/\D/g, "");
+      if (numbers.length <= 10) return numbers;
+      // Get 9 last digits and add 0 at the beginning to remove international code
+      e.target.value = "0" + numbers.slice(numbers.length - 9);
+    });
+    clearInterval(intervalID)
+  }
+}, 1000);
